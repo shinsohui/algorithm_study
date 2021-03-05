cb2955f585c18dca5300636f3d38a18560781b85
Shinsohui <ssh9601@dongguk.edu>
2021-03-05 10:35:05 +0900

    Initial Commit

---
+++ newc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  newc
+//
+//  Created by sohuishin on 2021/03/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
