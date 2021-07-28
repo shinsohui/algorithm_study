562642b749a5378e213539177d4c793117c82289
shinsohui <ssh9601@dongguk.edu>
2021-07-28 16:29:54 +0900

    약수 구하기

---
+++ 2501.py
@@ -0,0 +1,13 @@
+N, K = map(int, input().split())
+
+list = []
+
+for i in range(1,N+1, 1): 
+    if N%i == 0: 
+        list.append(i)
+
+if len(list) >= K:
+    print(list[k-1])   
+else:
+    print(0)
+
